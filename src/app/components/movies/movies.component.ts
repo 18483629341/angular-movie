da1cdb0b4c89395ce7e0dba38d8a0fb534890346
lisa091500 <714402934@qq.com>
2018-10-22 11:35:02 +0800

    construcor  渲染

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -24,15 +24,20 @@ export class MoviesComponent implements OnInit {
       this.id=params.id;
       this.name=params.name;
       //this.user['id']=params.id;
+      //这里路劲每跳转时，渲染一次
+      this.http.getMoviesByGenre(this.id).subscribe(res=>{
+        console.log(res);
+        this.movies=res.results;
+      })
     });
    }
 
-  ngOnInit() {
-    
-    this.http.getMoviesByGenre(this.id).subscribe(res=>{
-      console.log(res);
-      this.movies=res.results;
-    })
+  ngOnInit() {//这里只渲染一次，路劲跳转时，不渲染
+    //this.http.getMoviesByGenre(this.id).subscribe(res=>{
+    //  console.log(res);
+    //  this.movies=res.results;
+   // })
+   
     
   }
 
